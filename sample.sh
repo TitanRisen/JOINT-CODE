29fe93e28f90bb5146500c24704b63f8ce18c437
TitanRisen <thomas0@126.com>
2024-08-06 23:10:18 +0800

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,1 +1,1 @@
-python  main_for_UDA.py --root_dir ./data --dataset officehome --source Product    --target Art   --phase train --gpu 0  --steps 70000 --lambda_div 0.1
+python  main_for_UDA.py --root_dir ./data --dataset officehome --source Product    --target Clipart   --phase train --gpu 0  --steps 70000 --lambda_div 0.1
